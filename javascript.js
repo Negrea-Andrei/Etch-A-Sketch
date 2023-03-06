e8d5b8b98609a2a34b855256e42ad6bc8f37cf1d
Negrea-Andrei <negrea97@yahoo.com>
2023-03-06 21:06:22 +0200

    Made the butons interactive so that the grid size is changing dimensions

--- javascript.js
+++ javascript.js
@@ -1,12 +1,26 @@
 const grid_container = document.querySelector('.painting-grid');
-const size = document.querySelectorAll('.grid-size')
+const size = document.querySelector('.grid-size');
+const sizing = size.childNodes;
 
-function createGrid(rows) {
-    grid_container.style.setProperty('--grid-row', rows);
-    for(i = 0; i < rows * rows; i++) {
+function startGrid(number) {
+    grid_container.style.setProperty('--grid-row', number);
+    for(i = 0; i < number * number; i++) {
+        let block = document.createElement('div');
+        grid_container.appendChild(block).className="block";
+    }
+}
+    
+
+function createGrid(event) {
+    grid_container.innerHTML= '';
+    let number = event.target.classList[1];
+    grid_container.style.setProperty('--grid-row', number);
+    for(i = 0; i < number * number; i++) {
         let block = document.createElement('div');
         grid_container.appendChild(block).className="block";
     }
 }
 
-createGrid(32);
+startGrid(8);
+ 
+sizing.forEach(button => button.addEventListener('click', createGrid));
